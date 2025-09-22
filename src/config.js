e4e816a52b73f1952f8492f58bbee9397f96b654
GermanMetzger <metzgergerman@gmail.com>
2025-09-22 18:36:29 -0300

    librerias

--- src/config.js
+++ src/config.js
@@ -1,10 +1,17 @@
+import dotenv from "dotenv"
+dotenv.config();
+
 const config = {
-  port: process.env.PORT || 4003,
+  port: process.env.PORT,
   db: {
     host: process.env.DB_HOST || "localhost",
     user: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD || "54213",
     database: process.env.DB_NAME || "MediTurnos"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
   }
 }
 
