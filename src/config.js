f8aa7cd3c3ecd34c980a376f122f0890d8bc603f
GermanMetzger <metzgergerman@gmail.com>
2025-09-22 02:28:22 -0300

    limpieza

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 const config = {
   port: process.env.PORT || 4003,
   db: {
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "54213",
+    host: process.env.DB_HOST || "127.0.0.1",
+    user: process.env.DB_USER || "sqluser",
+    password: process.env.DB_PASSWORD || "Walo45785",
     database: process.env.DB_NAME || "MediTurnos"
   }
 }
